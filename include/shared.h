65b2294908c68ce935a1ab46ffa3286b5a973780
Facundo López Llopis <flopezllopis@itba.edu.ar>
2026-03-17 22:34:26 +0000

    feat: initialize args

---
+++ include/shared.h
@@ -0,0 +1,11 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+    #include <fcntl.h>
+    #include <sys/stat.h>
+    #include <stdio.h>
+    #include <sys/mman.h>
+
+    void * initializeShared(const * name, unsigned long size)
+
+#endif
